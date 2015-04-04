1ec51994058a79d0f90dfb7ad7274d4fd38e4ee9
Willie Long <willie.long@duke.edu>
2015-04-04 10:57:39 -0400

    vertical grid sim

--- gen_data_ece582MV.m
+++ gen_data_ece582MV.m
@@ -10,15 +10,17 @@
 % save('point_target_MV128fft.mat','rf_out','x','z','rf');
 % fprintf('Finished. \n')
 
-%% point grid simulation and MV beamform
+%% vertical grid simulation and MV beamform
 clear all; close all; clc;
 fprintf('Starting point grid simulation. \n')
 
-zpos = [35:10:55 30:5:60 42.5:2.5:47.5];
-xpos = [-5*ones(1,3) zeros(1,7)  5*ones(1,3)];
-sector = 20;
+% zpos = [35:10:55 30:5:60 42.5:2.5:47.5];
+% xpos = [-5*ones(1,3) zeros(1,7)  5*ones(1,3)];
+zpos = [42.5 45 47.5];
+xpos = zeros(1,length(zpos));
+sector = 14;
 [rf, rf_raw, bf_params, acq_params] = fieldLinearScan(xpos,zpos,sector);
-save('point_grid.mat','acq_params','bf_params','rf','rf_raw');
+save('point_gridv.mat','acq_params','bf_params','rf','rf_raw');
 [rf_out, x, z] = linearScanMV(rf,acq_params,bf_params,[],0);
-save('point_grid_MV128fft.mat','rf_out','x','z','rf');
+save('point_gridv_MV128fft.mat','rf_out','x','z','rf');
 fprintf('Finished. \n')

2a7bbf878ba1a333ea1562657dc48e8809df423a
Willie Long <Donatello@Wills-Macbook.local>
2015-04-05 19:41:50 -0400

    re-run sims with centered points

--- gen_data_ece582MV.m
+++ gen_data_ece582MV.m
@@ -26,19 +26,19 @@
 % fprintf('Finished. \n')
 
 %% horizontal grid simulation and MV beamform
-clear all; close all; clc;
-fprintf('Starting point grid simulation. \n')
-
-xpos = [-1 0 1];
-zpos = 45.*ones(1,length(xpos));
-sector = 14;
-[rf, rf_raw, bf_params, acq_params] = fieldLinearScan(xpos,zpos,sector);
-save('point_gridh1.mat','acq_params','bf_params','rf','rf_raw');
-[rf_out, x, z] = linearScanDR(rf,acq_params,bf_params);
-save('point_gridh1_DR.mat','rf_out','x','z'); clear rf_out x z
-[rf_out, x, z] = linearScanMVfast(rf,acq_params,bf_params,[],0);
-save('point_gridh1_MV128fft.mat','rf_out','x','z');
-fprintf('Finished. \n')
+% clear all; close all; clc;
+% fprintf('Starting point grid simulation. \n')
+% 
+% xpos = [-1 0 1];
+% zpos = 45.*ones(1,length(xpos));
+% sector = 14;
+% [rf, rf_raw, bf_params, acq_params] = fieldLinearScan(xpos,zpos,sector);
+% save('point_gridh1.mat','acq_params','bf_params','rf','rf_raw');
+% [rf_out, x, z] = linearScanDR(rf,acq_params,bf_params);
+% save('point_gridh1_DR.mat','rf_out','x','z'); clear rf_out x z
+% [rf_out, x, z] = linearScanMVfast(rf,acq_params,bf_params,[],0);
+% save('point_gridh1_MV128fft.mat','rf_out','x','z');
+% fprintf('Finished. \n')
 
 %% horizontal grid simulation and MV beamform
 clear all; close all; clc;

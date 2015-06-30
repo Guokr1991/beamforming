62e92440da4b5d729f1f210c4be031afd9279eb9
Willie Long <willie.long@duke.edu>
2015-06-30 11:02:16 -0400

    automated push

--- accessory/applyApod.m
+++ accessory/applyApod.m
@@ -1,4 +1,4 @@
-function [rf_out, rf_apo] = applyApod(rf,type,param)
+function [rf_out, rf_apo] = applyApod(rf,type,param,tag)
 % [rf_out, rf_apo] = applyApod(rf,'gauss',2.5)
 % 
 % rf - steered data pre-summation
@@ -7,7 +7,10 @@ function [rf_out, rf_apo] = applyApod(rf,type,param)
 % 
 % rf_out - summed apodized rf data
 % rf_apo - pre-summed apodized rf data
-    
+if nargin < 4
+    tag = 0;
+end
+
 L = size(rf,2); 
 
 switch type
@@ -19,9 +22,11 @@ end
 
 winmat = repmat(win',size(rf,1),1);
 
+if tag == 1
 figure
 plot(win)
 title('Apodization weights');
+end
 
 rf_apo = zeros(size(rf));
 for l = 1:size(rf,3); 
